542f254644d9707e3a346df0dc4e4c03d914bb39
lukehardcastle12 <lukehardcastle12@gmail.com>
2023-07-24 16:54:09 +0100

    Add remove book feature

--- library.js
+++ library.js
@@ -11,15 +11,17 @@ let myLibrary = [];
 //add a byttib ib each books display that allows it to be removed from the library
 //add a button that toggles the read status
 //for this create a function that toggles the status on the book prototype instance
+
+
+//WANTS
+//make add book a modal
+//rename filter to sort
 function Book(id, title, author, pages, isRead){
     this.id = id;
     this.title = title;
     this.author = author;
     this.pages = pages;
     this.isRead = isRead;
-}
-Book.prototype.toggleRead = function(){
-    Book.isRead = !Book.isRead
 }
 //starting data
 const theHobbit = new Book(1, 'the Hobbit', 'J. R. R. Tolkien', 295, true);
@@ -29,7 +31,18 @@ myLibrary.push(threeBodyProblem);
 const mobyDick = new Book(3, 'Moby Dick', 'Herman Melville', 427, true);
 myLibrary.push(mobyDick);
 createDisplay()
-
+//show the add book form when add book is clicked and hide when clicked again
+//defaults to hidden
+const showFormButton = document.querySelector('#show-form');
+const bookForm = document.querySelector('.bookform');
+bookForm.style.display = "none"
+showFormButton.addEventListener('click', function(){
+    if(bookForm.style.display === "none"){
+        bookForm.style.display = "block";
+    }else {
+        bookForm.style.display = "none";
+    }
+});
 //create card
 //populate card with holders for book data
 function createDisplay(){
@@ -37,6 +50,8 @@ function createDisplay(){
     for(i=0;i < myLibrary.length;i++){
         const card = document.createElement('div');
         card.classList.add('card');
+        card.setAttribute('id', (i+1));
+        
         const title = document.createElement('p');
         title.innerHTML = `Title: ${myLibrary[i].title}`;
         const author = document.createElement('p');
@@ -46,12 +61,13 @@ function createDisplay(){
         
         let readButton = document.createElement('button');
         myLibrary[i].isRead ? readButton.textContent = "Read" : readButton.textContent = "Not Read"
-        readButton.addEventListener('click', function(){
-            Book.toggleRead();
-        });
 
         const removeButton = document.createElement('button');
-        removeButton.textContent = "Delete Book"
+        removeButton.textContent = "Remove Book"
+        removeButton.addEventListener('click',function(event){
+            console.log(event.target.parentElement.id);
+            event.target.parentElement.remove();
+        });
 
         card.appendChild(title);
         card.appendChild(author);
@@ -64,6 +80,7 @@ function createDisplay(){
 //when the form is submited run this function to add the book
 const createBookButton = document.querySelector('#add-btn');
 createBookButton.addEventListener('click', () => addBookToLibrary());
+
 function addBookToLibrary(){
     const libraryDisplay = document.querySelector('.books');
     const titleText = document.querySelector('#book-title');
@@ -74,6 +91,7 @@ function addBookToLibrary(){
    
     const card = document.createElement('div');
     card.classList.add('card');
+    card.setAttribute('id', (myLibrary.length+1));
 
     const title = document.createElement('p');
     title.innerHTML = `Title: ${titleText.value}`;
@@ -85,14 +103,21 @@ function addBookToLibrary(){
     pages.innerHTML = `Length: ${pagesNumber.value} Pages`;
 
     let readButton = document.createElement('button');
-    isReadStatus ? readButton.textContent = "Read" : readButton.textContent = "Not Read";
-    readButton.addEventListener('click', function () {
-        Book.toggleRead();
-
-    });
+    readButton.setAttribute('id',(myLibrary.length + 1));
+    if(isReadStatus.checked === true){
+        readButton.innerHTML = "Read"
+    }else {
+        readButton.innerHTML = "Not Read";
+    }
     const removeButton = document.createElement('button');
-    removeButton.textContent = "Delete Book";
-
+    removeButton.textContent = "Remove Book";
+    removeButton.addEventListener('click', function(event){
+        console.log(event.target.parentElement.id);
+        event.target.parentElement.remove();
+        
+    });
+    const newBook = new Book((myLibrary.length + 1), titleText.value, authorText.value, pagesNumber.value, isReadStatus.checked);
+    myLibrary.push(newBook);
     card.appendChild(title);
     card.appendChild(author);
     card.appendChild(pages);
